8567f7bfdd29667d6909234c2469e13882ff055e
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-14 17:38:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   new_array = []
    string.each do |word| word.reverse
-     new_array << word
+     new_array.split << word
    end
    return new_array.join(" ")
 end 
